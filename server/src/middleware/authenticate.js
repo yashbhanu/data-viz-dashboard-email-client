d27b4c22da71aae89b3c7caa78a6266ec9047571
yashbhanushali24 <yash.bhanushali@code-b.dev>
2024-10-29 17:42:34 +0530

    added sign in sign up routes along with middleware

---
+++ server/src/middleware/authenticate.js
@@ -0,0 +1,17 @@
+export function authorize(req, res, next) {
+    try {
+        let token = req.get("authorization").split(" ")[1];
+        if (!token) {
+            return res.status(404).json({
+                success: false,
+                msg: "Token not found",
+            });
+        }
+        token = token.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        return res.status(403).json({ success: false, msg: error.message || 'Forbidden' });
+    }
+}
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,10 +1,31 @@
 import express from "express";
-
+import User from "../models/User";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 const userRouter = express.Router();
 
 userRouter.route("/signup").post(async (req, res) => {
   try {
-    console.log("signup");
+    const { email, password, name } = req.body;
+    if(!email || !password || !name) {
+      throw new Error('Enter Email & Password & Name');
+    }
+    let user = await User.findOne({email: email});
+    if(user){
+      throw new Error("Sorry a user with this email exists");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const encryptPass = await bcrypt.hash(password,salt);
+    user = await User.create({
+      name: name,
+      email: email,
+      password: encryptPass,
+    });
+
+    const authToken = jwt.sign({user}, process.env.JWT_SECRET);
+    res.status(201).json({authToken})
+
   } catch (error) {
     res.status(500).json({
       message: error.message || "Bad Request. Please Try Again",
@@ -14,7 +35,22 @@ userRouter.route("/signup").post(async (req, res) => {
 
 userRouter.route("/signin").post(async (req, res) => {
   try {
-    console.log("signin");
+    const { email, password } = req.body;
+    if(!email || !password) {
+      throw new Error('Enter Email & Password & Name');
+    }
+    let user = await User.findOne({email: email});
+    if(!user){
+      throw new Error("Incorrect Email or Password");
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare) {
+      throw new Error("Incorrect Email or Password");
+    }
+
+    const authToken = jwt.sign({user}, process.env.JWT_SECRET);
+    res.status(201).json({authToken})
   } catch (error) {
     res.status(500).json({
       message: error.message || "Bad Request. Please Try Again",
